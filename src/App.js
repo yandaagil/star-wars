488b6a191a5c72a1489685724dd0768f578f094d
Yanda Agil <yandaagil@gmail.com>
2023-08-04 12:32:32 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Main from "./layouts/Main";
+import People from "./routes/People";
+import PeopleDetail from "./routes/PeopleDetail";
+// TODO: answer here
+import MovieDetail from "./routes/MovieDetail";
+import Movies from "./routes/Movies";
+import PlanetDetail from "./routes/PlanetDetail";
+import Planets from "./routes/Planets";
+import NotFound from "./routes/NotFound";
+import "./App.css";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="star-wars" element={<Main />}>
+        <Route path="people">
+          <Route index element={<People />} />
+          <Route path=":id" element={<PeopleDetail />} />
+        </Route>
+        {/* TODO: answer here */}
+        <Route path="planets">
+          <Route index element={<Planets />} />
+          <Route path=":id" element={<PlanetDetail />} />
+        </Route>
+        <Route path="movies">
+          <Route index element={<Movies />} />
+          <Route path=":id" element={<MovieDetail />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default App;
